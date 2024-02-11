acc095c3a563e0ee8407db6442e619104e1e3afd
Felissi <felissistarry@gmail.com>
2024-02-12 01:16:43 +0800

    fix: align component test config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "cypress";
 
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import Vue from "@vitejs/plugin-vue";
-import Jsx from "@vitejs/plugin-vue-jsx";
-import { defineConfig as viteDefineConfig } from "vite";
-import { nuxtPaths, paths } from "./utils/parse-tsconfig";
+import vitestConfig from "./vitest.config";
 
 export default defineConfig({
   e2e: {
@@ -17,22 +13,7 @@ export default defineConfig({
     devServer: {
       framework: "vue",
       bundler: "vite",
-      viteConfig: viteDefineConfig({
-        plugins: [Vue(), Jsx(), vanillaExtractPlugin()],
-        test: {
-          globals: true,
-          environment: "jsdom",
-        },
-        resolve: {
-          alias: {
-            ...nuxtPaths,
-            ...paths,
-          },
-        },
-        css: {
-          postcss: { plugins: [require("tailwindcss"), require("autoprefixer")] },
-        },
-      }),
+      viteConfig: vitestConfig,
     },
   },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Jsx from "@vitejs/plugin-vue-jsx";
 import { nuxtPaths, paths } from "./utils/parse-tsconfig";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 export default defineConfig({
-  plugins: [Vue(), Jsx()],
+  plugins: [Vue(), Jsx(), vanillaExtractPlugin()],
   test: {
     globals: true,
     environment: "jsdom",
@@ -15,5 +16,8 @@ export default defineConfig({
       ...nuxtPaths,
       ...paths,
     },
+  },
+  css: {
+    postcss: { plugins: [require("tailwindcss"), require("autoprefixer")] },
   },
 });
